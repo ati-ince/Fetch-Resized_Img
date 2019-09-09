2dfcda56d042e974682f8685d472a8de5e86a42c
inceabdullah <pax.castrensis@gmail.com>
2019-09-09 14:19:35 -0700

    timeout
    30sn lik time out ekledim

--- Fetch_Resized_ImgName.py
+++ Fetch_Resized_ImgName.py
@@ -7,14 +7,26 @@ def Fetch_Resized_ImgName(url, w_len = 200):
   # Fetch_Resized_ImgName(url) -> len(Fetch_Resized_ImgName(url)):4, 1-> w, 2->h, 3->filename, 4->(w,h)
 
   quoted_url = "http://haber.ozguruygulama.com/fetch_img_with_rszd?img_url=%s&w_len=%d" % (quote(url), w_len)
-  r = requests.get(quoted_url, allow_redirects=True)
+  
+  r_get_false = False
+  
+  try:
+  
+    r = requests.get(quoted_url, allow_redirects=True, timeout=30)
   
   # jsone_encoded = r.content.decode('utf-8')
   
-  try:
-    json_decoded = json.loads(r.text) # jsone_encoded)
   except:
-    json_decoded = json.loads("""[{"width":"200","height":"166","filename":"http_haber.ozguruygulama.com_static_h_news.png"}]""")  
+    
+    json_decoded = json.loads("""[{"width":"200","height":"166","filename":"http_haber.ozguruygulama.com_static_h_news.png"}]""") 
+    r_get_false = True
+  
+  if r_get_false == False:
+  
+    try:
+      json_decoded = json.loads(r.text) # jsone_encoded)
+    except:
+      json_decoded = json.loads("""[{"width":"200","height":"166","filename":"http_haber.ozguruygulama.com_static_h_news.png"}]""")  
   
   res_width = json_decoded[0]["width"]
   res_height = json_decoded[0]["height"]
